b3f6eb0641134741880d72d7676ce276d9f8f91c
muzz707 <muzaffarm58@gmail.com>
2020-06-12 12:25:00 -0400

    init2

--- bruda/main.cpp
+++ bruda/main.cpp
@@ -7,7 +7,7 @@ int main()
 {
 
     int u;
-    u = connectbyportint("www.google.com", 80);
+    u = connectbyportint("www.googlrrre.com", 80);
     cout << "Hello worldasd!" << u << endl;
     return 0;
 }
