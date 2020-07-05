c5456a10072453228e83bbbda941c48815a433e5
Ernie <jasonernie03@gmail.com>
2020-07-05 12:05:44 -0400

    1.12

--- bullinServer/main.cpp
+++ bullinServer/main.cpp
@@ -23,68 +23,75 @@ int main(int argc, char** argv) {
     
     // handle command line arguments:
     int option;
-    string input = "";
+    string lastInput = "";
 
     // Retrieve the (non-option) argument:
     if ( (argc <= 1) || (argv[argc-1] == NULL) || (argv[argc-1][0] == '-') ) {  // there is NO input...
         cerr << "No argument provided!" << endl;
-        //return 1;
+        return 1;
     }
     else {  // there is an input...
-        input = argv[argc-1];
-        // TODO: deal with  non-option arguments: https://stackoverflow.com/questions/18079340/using-getopt-in-c-with-non-option-arguments
+        lastInput = argv[argc-1];
     }
 
-    cout << "input = " << input << endl;
-    cout << "argv的原貌" << argv[argc-1]  << endl;
+    cout << "lastInput = " << lastInput << endl;
 
     // Shut GetOpt error messages down (return '?'):
     opterr = 0;
 
     // Retrieve the options:
-    while ( (option = getopt(argc, argv, "b:c:T:t:p:s:fdh")) != -1 ) {  // for each option...
-        switch ( option ) {
-            case 'b':
-                cout << "ToDo: change bbfile new name" << optarg << endl;
-                break;
-            case 'c':
-                cout << "ToDo: change bbserv.conf  new name" << optarg << endl;
-                break;
-            case 'T':
-                Tmax = stoi(optarg);
-                cout << "do things with T para, new Tmax now is: " << Tmax << endl;
-                break;
-            case 't':
-                Tmax = stoi(optarg);
-                cout << "do things with T para, new Tmax now is: " << Tmax << endl;
-                break;
-            case 'p':
-                bp = stoi(optarg);
-                cout << "do things with bp para, new bp now is: " << bp << endl;
-                break;
-            case 's':
-                sp = stoi(optarg);
-                cout << "do things with sp para, new bp now is: " << sp << endl;
-                break;
-            case 'f':
-                d = false;
-                cout << "ToDo: change value of d in conf file" << endl;
-                break;
-            case 'd':
-                D = true;
-                cout << "ToDo: change value of D in conf file" << endl;
-                break;
-            case '?':  // unknown option...
-                    cerr << "Unknown option: '" << char(optopt) << "'!" << endl;
-                break;
-            case 'h':
-                printhelpFunction();
-                return EXIT_SUCCESS;
-            default:
-                cout << "ToDo: handle non-switch argument " << endl;
-                cout << "ToDo: handle error" << endl;
+    while (optind < argc) {
+        if ( (option = getopt(argc, argv, "b:c:T:t:p:s:fdh")) != -1 ) {  // for each option...
+            switch ( option ) {
+                case 'b':  // change bbfile name
+                    cout << "ToDo: change bbfile new name" << optarg << endl;
+                    break;
+                case 'c':  // change config file name
+                    cout << "ToDo: change bbserv.conf  new name" << optarg << endl;
+                    break;
+                case 'T':  // overide Tmax number, preallocated threads
+                    Tmax = stoi(optarg);
+                    cout << "do things with T para, new Tmax now is: " << Tmax << endl;
+                    break;
+                case 't':  // overide Tmax number, preallocated threads
+                    Tmax = stoi(optarg);
+                    cout << "do things with T para, new Tmax now is: " << Tmax << endl;
+                    break;
+                case 'p':  // client server port number
+                    bp = stoi(optarg);
+                    cout << "do things with bp para, new bp now is: " << bp << endl;
+                    break;
+                case 's': // server port number
+                    sp = stoi(optarg);
+                    cout << "do things with sp para, new bp now is: " << sp << endl;
+                    break;
+                case 'f':   // start daemon...
+                    d = false;
+                    cout << "ToDo: change value of d in conf file" << endl;
+                    break;
+                case 'd':   // debug mode...
+                    D = true;
+                    cout << "ToDo: debug mode" << endl;
+                    break;
+                case '?':  // unknown option...
+                        cerr << "Unknown option: '" << char(optopt) << "'!" << endl;
+                    break;
+                case 'h':  // help menu
+                    printhelpFunction();
+                    return EXIT_SUCCESS;
+                default:
+                    cout << "ToDo: handle non-switch argument " << endl;
+                    cout << "ToDo: handle error" << endl;
+                    break;
+            }
+        } else { // handle host:post  peers          and other start options
+            cout << "handle non-switch argument are: " << argv[optind] << endl;
+            optind++;
         }
     }
+    
+    
+    // change the value in the config file if the value for d is true then we need to start the server
     
     
     
